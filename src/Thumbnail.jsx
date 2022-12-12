735486f49ab09debbcf7504d0e5d30429f7082f1
CoralieCC <coraliecharbonnay@gmail.com>
2022-12-12 10:35:47 +0100

    margin

--- src/Thumbnail.jsx
+++ src/Thumbnail.jsx
@@ -89,7 +89,7 @@ export const Thumbnail = ({
 
   const iframeShade = {
     position: "relative",
-    margin: "0 0 -190px 0",
+    margin: `0 0 ${-Math.abs(calculatedSize.height)} 0`,
     width: calculatedSize.width,
     height: calculatedSize.height,
     zIndex: "10",
